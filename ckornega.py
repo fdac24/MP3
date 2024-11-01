a386153fa71b2c730fd21a9e62b61baf0c54735e
CalebKornegay <caleb.kornegay@gmail.com>
2024-11-01 18:43:48 -0400

    Adding bibs and fixing some URLs

--- ckornega.py
+++ ckornega.py
@@ -15,37 +15,39 @@ postGHMain: str = 'blob/main/README.md'
 extU: URLExtract = URLExtract()
 DOIpattern: str = r'\b(10\.\d{4,9}\/[-._;()/:A-Z0-9]+)\b/i'
 #r1\b(10[.][0-9]{4,}(?:[.][0-9]+)*/(?:(?!["&\'<>])[[:graph:]])+)\b'
+BIBpattern: str = r'@\w+{\n*.*\n*?}' # Found online
 
 def extractURLs(c: str) -> list:
-    res = extU.find_urls(c)
-    return res
+    return extU.find_urls(c)
 
 def extractDOIs(c: str) -> list:
-    res = re.findall(DOIpattern, c)
-    return res
+    return re.findall(DOIpattern, c)
+
+def extractBIBS(c: str) ->  list:
+    return re.findall(BIBpattern, c, re.DOTALL)
 
 fo = gzip.open(f"output/{utid}.json.gz", 'w')
 
 def run(tp: str):
-    global called
     post0 = post if tp != 'source' else postGHMaster
  
     with open(f"input/{utid}_{tp}", 'r', encoding='utf8', buffering=16384) as f:
         for line in f:
             line = line.strip()
+            add: str = ""
             if tp == 'source':
-                npapers,line = line.split(';');
+                npapers, line = line.split(';');
+                add = "/" if line[-1] != "/" else ""
 
-            print(line)
-            url = baseURL[tp] + line
-            url = url + "/" if url[-1] != "/" else "" + post0
+            url = baseURL[tp] + line + add
+            url = url + post0
             
             try:
                 r = requests.get(url)
                 if r.status_code == 404:
                     if tp != 'source': continue # failure
-                    url = baseURL[tp] + line
-                    url = url + "/" if url[-1] != "/" else "" + postGHMain
+                    url = baseURL[tp] + line  + add
+                    url = url + postGHMain
                     try:
                         r = requests.get(url)
                         if r.status_code == 404: continue # failure
@@ -55,14 +57,16 @@ def run(tp: str):
             content = r.text;
             urls = extractURLs(content)
             dois = extractDOIs(content)
-            
+            bibs = extractBIBS(content)
+
             res = {
                 'ID': line,
                 'type': tp,
                 'url': url,
                 'content': content,
                 'links': urls,
-                'dois': dois
+                'dois': dois,
+                'bibs': bibs
                 }
             
             out = json.dumps(res, ensure_ascii=False)
