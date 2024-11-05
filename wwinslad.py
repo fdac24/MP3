1004ab8109a3a885c020c889a5ca6fc16e9de469
wwinslade <williamwinslade1@gmail.com>
2024-11-05 11:03:05 -0500

    Finished

--- wwinslad.py
+++ wwinslad.py
@@ -7,8 +7,8 @@ import sys, gzip
 utid = 'wwinslad'
 base= { 'model':'https://huggingface.co/', 'data': 'https://huggingface.co/datasets/', 'source': 'https://' }
 post = '/raw/main/README.md'
-postGH = 'blob/master/README.md' # or it could be 'blob/main/README.md'
-postGH_alt = 'blob/main/README.md'
+postGH = 'blob/master/README.md' 
+# In my testing, GitHub automatically redirects to main if master doesn't exist and displays README.md anyway
 
 extU = URLExtract()
 DOIpattern = r'\b(10\.\d{4,9}\/[-._;()/:A-Z0-9]+)\b/i'
@@ -22,10 +22,11 @@ def extractDOIs (c):
  res = re.findall (DOIpattern, c)
  return res
 
+# Kind of finnicky since regex doesn't like nested braces, but works good enough for most cases
 def extractBibs(c):
-   BibtexPattern = r'@(\w+)\s*{\s*([^,]+),\s*([^}]*)}'
-   res = re.findall (BibtexPattern, c)
-   return res
+ BibtexPattern = r'@(\w+)\s*{\s*([^,]+),\s*([^}]*)}'
+ res = re.findall (BibtexPattern, c)
+ return res
 
 fo = gzip.open(f"output/{utid}.json.gz", 'w')
 
@@ -39,31 +40,33 @@ def run (tp):
     post0 = postGH
    print(line)
    url = base[tp] + f"{line}{post0}"
-   r = requests.get(url)
+   r = requests.get (url)
    content = r.text
 
    #github returns repos that do not exist, need to detect that here
-   if 'Page not found' in content:
-     print(f"Repo not found: {line}")
-     res = { 'ID': line, 'type': tp, 'url': url, 'content': 'Repo not found', 'links': [], 'dois': [], 'bibs': [] }
-     out = json.dumps(res, ensure_ascii=False)
-     fo.write((out+"\n").encode())
-     continue
+   if 'This is not the web page you are looking for' in content:
+    # This means the repo doesn't exist
+    print(' --> Invalid repo: 404 error on repo path')
+    res = { 'ID': line, 'type': tp, 'url': url, 'content': 'Invalid repo: 404 error on repo path', 'links': [], 'dois': [], 'bibs': [] }
+   elif 'File not found' in content:
+    # This means the repo exists but doesn't have a README.md file
+    print(' --> Invalid file: 404 error on file path')
+    res = { 'ID': line, 'type': tp, 'url': url, 'content': 'Invalid file: 404 error on file path', 'links': [], 'dois': [], 'bibs': [] }
+   else:    
+    # Can proceed normally
+    urls = extractURLs(content)
+    dois = extractDOIs(content)
+    bibs = extractBibs(content)
+    res = { 'ID': line, 'type': tp, 'url': url, 'content': content, 'links': urls, 'dois': dois, 'bibs': bibs }
+   
+   # Note: I found in my testing that GitHub automatically redirects to default branch if you try and navigate to one that doesn't exist
+   # so there was no need for separate logic for "/blob/master/README.md" vs "/blob/main/README.md"
 
-   #github when you give master instead of main, that might cause issues as well
-   if 'File not found' in content:
-    print(f"404, switching to main: {line}")
-    url = base[tp] + f"{line}{postGH_alt}"
-    r = requests.get(url)
-    content = r.text
-
-   urls = extractURLs(content)
-   dois = extractDOIs(content)
-   bibs = extractBibs(content)
-   res = { 'ID': line, 'type': tp, 'url': url, 'content': content, 'links': urls, 'dois': dois, 'bibs': bibs }
    out = json.dumps(res, ensure_ascii=False)
    fo.write((out+"\n").encode())
 
 run('model')
 run('data')
 run('source')
+
+fo.close()
