a0e3dd7a94ac9b5933a924246b7a3ffd25abe018
nightOwl <ConnectWithFarzin@gmail.com>
2024-11-05 22:29:37 -0500

    Final version
    Made some changes to file path

--- fgholamr.py
+++ fgholamr.py
@@ -8,11 +8,13 @@ utid = 'fgholamr'
 base= { 'model':'https://huggingface.co/', 'data': 'https://huggingface.co/datasets/', 'source': 'https://' }
 post = '/raw/main/README.md'
 postGH = '/blob/master/README.md' # or it could be 'blob/main/README.md'
-postGHMain = '/blob/main/README.md'
+postGHMain = '/blob/main/README.md' #changed to raw in order to avoid massive html inputs in content
 
 extU = URLExtract()
 DOIpattern = r'\b(10\.\d{4,9}\/[-._;()/:A-Z0-9]+)\b/i'
-#r1\b(10[.][0-9]{4,}(?:[.][0-9]+)*/(?:(?!["&\'<>])[[:graph:]])+)\b'
+
+Bibpattern = r'@(\w+)\s*{([^@]*)}'
+
 
 def extractURLs (c):
  res = extU.find_urls (c)
@@ -20,6 +22,10 @@ def extractURLs (c):
 
 def extractDOIs (c):
  res = re.findall (DOIpattern, c)
+ return res
+
+def extractBibs(c): # returning bibpattern
+ res = re.findall(Bibpattern, c)
  return res
 
 fo = gzip.open(f"output/{utid}.json.gz", 'w')
@@ -38,15 +44,18 @@ def run (tp):
    print(url) #checking for errors in url
    r = requests.get (url)
    print(post0)
-   if r.status_code == 404:
-      print(f"404 Error at {url} try main") ## Id url error, try switching to Main
+   if r.status_code != 200:  # included 200 to get a larger number of errors
+   #if r.status_code != 404:
+      print(f"{r.status_code}, Error at {url} try main") ## Id url error, try switching to Main
       post0=postGHMain
       #print(f"updated readme to main", post0) 
+      
+      
       
       url = base[tp] + f"{line}{post0}"
       print(f"updated master to main new url is: {url}") #checking correct url implementation
       r = requests.get (url) # reinit request
-      if r.status_code == 404: # Empty repo move to next repo
+      if r.status_code != 200: # Empty repo move to next repo
          print("Empty Master and Main readme")
          continue
       
@@ -57,7 +66,8 @@ def run (tp):
    #github when you give master instead of main, that might cause issues as well
    urls = extractURLs(content)
    dois = extractDOIs(content)
-   res = { 'ID': line, 'type': tp, 'url': url, 'content': content, 'links': urls, 'dois': dois }
+   bibs = extractBibs(content) 
+   res = { 'ID': line, 'type': tp, 'url': url, 'content': content, 'links': urls, 'dois': dois ,'bib': bibs}
    out = json.dumps(res, ensure_ascii=False)
    fo.write((out+"\n").encode())
 
