27eee952628f22e3ba061947c8f1407d223dbaa8
Randy Lin <rlin8@vols.utk.edu>
2024-11-06 16:48:34 -0500

    commit

---
+++ rlin8.py
@@ -0,0 +1,54 @@
+import json, re
+import requests
+from urlextract import URLExtract
+import sys, gzip
+
+
+utid = 'rlin8'
+base= { 'model':'https://huggingface.co/', 'data': 'https://huggingface.co/datasets/', 'source': 'https://' }
+post = '/raw/main/README.md'
+postGH = 'blob/master/README.md' # or it could be 'blob/main/README.md'
+
+extU = URLExtract()
+DOIpattern = r'\b(10\.\d{4,9}\/[-._;()/:A-Z0-9]+)\b/i'
+#r1\b(10[.][0-9]{4,}(?:[.][0-9]+)*/(?:(?!["&\'<>])[[:graph:]])+)\b'
+
+def extractURLs (c):
+ res = extU.find_urls (c)
+ return res
+
+def extractDOIs (c):
+ res = re.findall (DOIpattern, c)
+ return res
+
+def extractBIBs (c):
+ bibs = re.findall(r'@(\w+)\{([^,]+),\s*(.*?)\}', c)
+ return bibs
+
+
+fo = gzip.open(f"output/{utid}.json.gz", 'w')
+
+def run (tp):
+ post0 = post
+ with open(f"input/{utid}_{tp}", 'r') as f:
+  for line in f:
+   line = line.strip ()
+   if tp == 'source':
+    (npapers,line) = line.split(';')
+    post0 = postGH
+   print(line)
+   url = base[tp] + f"{line}{post0}"
+   r = requests.get (url)
+   content = r.text
+   #github returns repos that do not exist, need to detect that here
+   #github when you give master instead of main, that might cause issues as well
+   urls = extractURLs(content)
+   dois = extractDOIs(content)
+   bibs = extractBIBs(content)
+   res = { 'ID': line, 'type': tp, 'url': url, 'content': content, 'links': urls, 'dois': dois, 'bibs': bibs }
+   out = json.dumps(res, ensure_ascii=False)
+   fo.write((out+"\n").encode())
+
+run('model')
+run('data')
+run('source')
