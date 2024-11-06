2aa5d774af43bd814b6eaaa58102d4d644ac171d
elielieli1234 <117529278+ElijahDoesCS@users.noreply.github.com>
2024-11-06 18:40:26 -0500

    Moved code to .ipynb w/ netid

--- example.py
+++ example.py
@@ -4,35 +4,26 @@ from urlextract import URLExtract
 import sys, gzip
 
 
-utid = 'edayney' # For output file
-
-# Base URLs for different repository types
+utid = 'audris'
 base= { 'model':'https://huggingface.co/', 'data': 'https://huggingface.co/datasets/', 'source': 'https://' }
-
-# README paths for huggingface and github respectively
 post = '/raw/main/README.md'
 postGH = 'blob/master/README.md' # or it could be 'blob/main/README.md'
 
-# Store URLExtract object for use in URL extraction functions
 extU = URLExtract()
 DOIpattern = r'\b(10\.\d{4,9}\/[-._;()/:A-Z0-9]+)\b/i'
 #r1\b(10[.][0-9]{4,}(?:[.][0-9]+)*/(?:(?!["&\'<>])[[:graph:]])+)\b'
 
-# Extracts all URLs in the content
 def extractURLs (c):
  res = extU.find_urls (c)
  return res
 
-# Extracts all Digital Object Identifiers in the content
 def extractDOIs (c):
  res = re.findall (DOIpattern, c)
  return res
 
-# Functional output? Sends output to json file
 fo = gzip.open(f"output/{utid}.json.gz", 'w')
 
 def run (tp):
- # Used to build the path to the README files
  post0 = post
  with open(f"input/{utid}_{tp}", 'r') as f:
   for line in f:
@@ -41,12 +32,9 @@ def run (tp):
     (npapers,line) = line.split(';');
     post0 = postGH
    print(line)
-   
-   # Build out the URL to the README file
    url = base[tp] + f"{line}{post0}"
    r = requests.get (url)
-   content = r.text
-   
+   content = r.text;
    #github returns repos that do not exist, need to detect that here
    #github when you give master instead of main, that might cause issues as well
    urls = extractURLs(content)
@@ -56,8 +44,5 @@ def run (tp):
    fo.write((out+"\n").encode())
 
 run('model')
-print("FINISHED MODEL PORTION")
 run('data')
-print("FINISHED DATA PORTION")
 run('source')
-print("FINISHED SOURCE PORTION")
