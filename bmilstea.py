00eed34c9e2226fbc1d07c076a11ac0b25d5e4e5
Blake Russell Milstead <112974709+BlakeMilstead@users.noreply.github.com>
2024-11-06 20:56:51 -0500

    Add files via upload

---
+++ bmilstea.py
@@ -0,0 +1,48 @@
+import json, re
+import requests
+from urlextract import URLExtract
+import sys, gzip
+
+
+utid = 'bmilstea'
+base= { 'model':'https://huggingface.co/', 'data': 'https://huggingface.co/datasets/', 'source': 'https://' }
+post = '/raw/main/README.md'
+postGH = 'blob/main/README.md' # or it could be 'blob/main/README.md'
+
+extU = URLExtract()
+DOIpattern = r'\b(10\.\d{4,9}\/[-._;()/:A-Z0-9]+)\b/i'
+#r1\b(10[.][0-9]{4,}(?:[.][0-9]+)*/(?:(?!["&\'<>])[[:graph:]])+)\b'
+
+def extractURLs (c):
+ res = extU.find_urls (c)
+ return res
+
+def extractDOIs (c):
+ res = re.findall (DOIpattern, c)
+ return res
+
+fo = gzip.open(f"output/{utid}.json.gz", 'w')
+
+def run (tp):
+ post0 = post
+ with open(f"input/{utid}_{tp}.txt", 'r', encoding='utf-8') as f:
+  for line in f:
+   line = line.strip ()
+   if tp == 'source':
+    (npapers,line) = line.split(';');
+    post0 = postGH
+   print(line)
+   url = base[tp] + f"{line}{post0}"
+   r = requests.get (url)
+   content = r.text;
+   #github returns repos that do not exist, need to detect that here
+   #github when you give master instead of main, that might cause issues as well
+   urls = extractURLs(content)
+   dois = extractDOIs(content)
+   res = { 'ID': line, 'type': tp, 'url': url, 'content': content, 'links': urls, 'dois': dois }
+   out = json.dumps(res, ensure_ascii=False)
+   fo.write((out+"\n").encode())
+
+run('model')
+run('data')
+run('source')
