efec2b229901853c7c66c15d99cfdca06d2b9c10
ddelrosa <ddelrosa@vols.utk.edu>
2024-11-06 23:56:23 -0500

    finish mp3

--- ddelrosa.py
+++ ddelrosa.py
@@ -1,3 +1,7 @@
+# Dylan DelRosa
+# CS445
+# MP3
+
 import json, re
 import requests
 from urlextract import URLExtract
@@ -5,16 +9,16 @@ import sys, gzip
 
 
 utid = 'ddelrosa'
-base= { 'model':'https://huggingface.co/', 'data': 'https://huggingface.co/datasets/', 'source': 'https://' }
+base = { 'model':'https://huggingface.co/', 'data': 'https://huggingface.co/datasets/', 'source': 'https://' }
 post = '/raw/main/README.md'
-postGH_master = 'blob/master/README.md' # or it could be 'blob/main/README.md'
+postGH_master = '/blob/master/README.md' # or it could be 'blob/main/README.md'
 postGH_main = 'blob/main/README.md'
 
 extU = URLExtract()
 DOIpattern = r'\b(10\.\d{4,9}\/[-._;()/:A-Z0-9]+)\b/i'
-#r1\b(10[.][0-9]{4,}(?:[.][0-9]+)*/(?:(?!["&\'<>])[[:graph:]])+)\b'
-BIBpattern = r'@.*?{.*?}' 
-# BIBpattern = r'\b(10[.][0-9]{4,}(?:[.][0-9]+)*/(?:(?!["&\'<>])[[:graph:]])+)\b'
+# BIBpattern : str = r'\b(10[.][0-9]{4,}(?:[.][0-9]+)*/(?:(?!["&\'<>])[[:graph:]])+)\b'
+
+BIBpattern: str = r'@.*?{.*?}'
 
 def extractURLs (c):
  return extU.find_urls (c)
@@ -23,9 +27,10 @@ def extractDOIs (c):
  return re.findall (DOIpattern, c)
 
 def extractBIBs (c):
- return re.findall (BIBpattern, c, re.DOTALL)
+  return re.findall (BIBpattern, c, re.DOTALL)
 
 fo = gzip.open(f"output/{utid}.json.gz", 'w')
+
 def run (tp):
  post0 = post
  post1 = post
@@ -33,39 +38,28 @@ def run (tp):
   for line in f:
    line = line.strip ()
    if tp == 'source':
-    (npapers,line) = line.split(';')
+    (npapers,line) = line.split(';');
     post0 = postGH_master
     post1 = postGH_main
-   print(f"Doing stuff: {line}")
+   print(line)
    url = base[tp] + f"{line}{post0}"
    r = requests.get (url)
-   
-#github returns repos that do not exist, need to detect that here
-   if r.status_code != 200: 
+# see if there is an error
+   if r.status_code == 404:
       if tp != 'source':
          continue
-
-      print (f"URL {url} error is {r.status_code} Trying again")
-#github when you give master instead of main, that might cause issues as well
+# try and use 'blob/main/README.md' instead of 'blob/master/README.md'
       url1 = base[tp] + f"{line}{post1}"
-      r = requests.get (url1)
-      if r.status_code != 200:
-         print (f"Error still persiting for URL: {url1} error is {r.status_code}")
+      r = requests.get(url1)
+# if still an error continue on
+      if r.status_code == 404:
          continue
-       
-   content = r.text
+      
+   content = r.text;
    urls = extractURLs(content)
    dois = extractDOIs(content)
    bibs = extractBIBs(content)
-   content = content.replace('\n', ' ')
-   # bibs = bibs.replace('\n', ' ')
-   res = { 'ID': line,
-           'type': tp, 
-           'url': url, 
-           'content': content, 
-           'links': urls, 
-           'dois': dois,
-           'bibs': bibs }
+   res = { 'ID': line, 'type': tp, 'url': url, 'content': content, 'links': urls, 'dois': dois, 'bibs': bibs }
    out = json.dumps(res, ensure_ascii=False)
    fo.write((out+"\n").encode())
 
