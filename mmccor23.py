128fbe7fccc8434ae7a04a625d57980e7808d26d
mikjmcc26 <mmccor23@vols.utk.edu>
2024-11-12 15:27:03 -0500

    Fixing bad output file

--- mmccor23.py
+++ mmccor23.py
@@ -1,82 +1,86 @@
 import json, re
-import os
 import requests
 from urlextract import URLExtract
 import sys, gzip
 
+
 utid = 'mmccor23'
-base = {
-    'model': 'https://huggingface.co/', 
-    'data': 'https://huggingface.co/datasets/', 
-    'source': 'https://github.com/'
-}
+base = { 'model': 'https://huggingface.co/', 
+        'data': 'https://huggingface.co/datasets/', 
+        'source': 'https://' }
+
 post = '/raw/main/README.md'
-postGH = 'blob/master/README.md'
+postGH = 'blob/master/README.md'  # or it could be 'blob/main/README.md'
+postGH_main = 'blob/main/README.md'
 
 extU = URLExtract()
 DOIpattern = r'\b(10\.\d{4,9}\/[-._;()/:A-Z0-9]+)\b/i'
+bib_pattern = r"@([a-zA-Z]+)\{([^}]+)\}"
 
-output_dir = 'output'
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
-
-def extractURLs (c):
+# Get urls
+def extractURLs(c):
     res = extU.find_urls(c)
     return res
 
-def extractDOIs (c):
+# Get DOIs
+def extractDOIs(c):
     res = re.findall(DOIpattern, c)
     return res
 
-def extract_bibs(c):
-    res = re.findall(r'@\w+\s*{[^}]+}', c)
+# Get bibs
+def extractbibs(c):
+    res = re.findall(bib_pattern, c)
     return res
 
-fo = gzip.open(f"output/{utid}.json.gz", 'w')  
 
-def run (tp):
-    post0 = post  
-    with open(f"input/{utid}_{tp}", 'r') as f:  
+fo = gzip.open(f"output/{utid}.json.gz", 'w')
+
+
+def run(tp):
+    post0 = post
+    
+    with open(f"input/{utid}_{tp}", 'r') as f:
         for line in f:
             line = line.strip()
             if tp == 'source':
                 (npapers, line) = line.split(';')
                 post0 = postGH
-            
-            print(f"Grabbing - {line}")
-            
+
+            print(line)
             url = base[tp] + f"{line}{post0}"
             r = requests.get(url)
-            
-            # GitHub returns repos that do not exist, need to detect that here
-            # GitHub when you give master instead of main, that might cause issues as well
-            
-            # Check if the URL returns a 404 error
-            if r.status_code == 404 and tp == 'source':
-            # Attempt to use 'main' instead of 'master' if the original URL fails
-               url = url.replace('blob/master', 'blob/main')
-               r = requests.get(url)
 
-            if r.status_code == 404:
-               print(f"Error - The repo at {url} does not exist or could not be found.")
-               continue
-      
-            c = r.text
-            urls = extractURLs(c)
-            dois = extractDOIs(c)
-            bibs = extract_bibs(c)
-            result = {
-                    'id': line,
-                    'type': tp,
-                    'url': url,
-                    'content': c,
-                    'links': urls,
-                    'dois': dois,
-                    'bibs': bibs
-            }
-            out = json.dumps(result, ensure_ascii=False)
+            # If it recieves an error code
+            if r.status_code != 200:
+                # Try with main
+                post0 = postGH_main
+                url = base[tp] + f"{line}{post0}"
+                r = requests.get(url)
+                # If it still gets an error code, only print out these values. 
+                if r.status_code != 200:
+                    print(f"Error: Could not access {url}")
+                    res = {'ID': line, 
+                           'type': tp, 
+                           'url': url, }
+            
+            # Otherwise continue as regular file 
+            else:
+              content = r.text
+              urls = extractURLs(content)
+              dois = extractDOIs(content)
+              bibs = extractbibs(content)
+              res = {'ID': line, 
+                     'type': tp, 
+                     'url': url, 
+                     'content': content, 
+                     'links': urls, 
+                     'dois': dois, 
+                     'bibs':bibs }
+
+            out = json.dumps(res, ensure_ascii=False)
             fo.write((out + "\n").encode())
 
 run('model')
 run('data')
 run('source')
+fo.close()
