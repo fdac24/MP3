6d8c27597cddc5189b7e56d48b19b39fbe4e63d8
Md. Saif Hassan Onim <onim.hassan16@gmail.com>
2024-11-12 15:32:28 -0500

    fixed the no content issue

--- monim.py
+++ monim.py
@@ -13,15 +13,14 @@ import sys, gzip
 from tqdm import tqdm
 
 utid = 'monim'
-base= { 'model':'https://huggingface.co/',
-       'data': 'https://huggingface.co/datasets/', 'source': 'https://' }
+base= { 'model':'https://huggingface.co/', 'data': 'https://huggingface.co/datasets/', 'source': 'https://' }
 post = '/raw/main/README.md'
 postGH = 'blob/master/README.md'
 postGH_1 =  'blob/main/README.md' #handling the case for main/master
 
 extU = URLExtract()
 DOIpattern = r'\b(10\.\d{4,9}\/[-._;()/:A-Z0-9]+)\b/i'
-bibPattern = r'@(\w+)\s*\{\s*([^,]+),\s*(.*?)\s*\}\s*' #bonus problem
+bibPattern = r'@(\w+)\{[^}]+\}' #bonus problem
 
 def extractURLs (c):
  res = extU.find_urls (c)
@@ -31,50 +30,47 @@ def extractDOIs (c):
  res = re.findall (DOIpattern, c)
  return res
 
-def extractBibs (c): #for bonus
+def extractBibs (c):
  res = re.findall (bibPattern, c)
  return res
 
 fo = gzip.open(f"output/{utid}.json.gz", 'w+')
 
 def run (tp):
- post0 = post
- post1 = post
- with open(f"input/{utid}_{tp}", 'r') as f:
-  for line in tqdm(f):
-   line = line.strip ()
-   if tp == 'source':
-    (npapers,line) = line.split(';');
-    post0 = postGH
-    post1 = postGH_1
-   print(line)
-   url = base[tp] + f"{line}{post0}"
-   url_1 = base[tp] + f"{line}{post1}"
-   try:
-    r = requests.get (url)
-    content = r.text
-    try:
-      r_1 = requests.get (url_1)
-      content_1 = r_1.text
-      content = content_1
-    except:
-      continue
+   post0 = post
+   with open(f"input/{utid}_{tp}", 'r') as f:
+      for line in tqdm(f):
+         line = line.strip ()
+         if tp == 'source':
+            (npapers,line) = line.split(';');
+            post0 = postGH
+         print(line)
+         url = base[tp] + f"{line}{post0}"
+         try:
+            r = requests.get (url)
+         except:
+            continue
+         if r.status_code != 200: 
+            if tp != 'source':
+               continue
+            else:
+               url_1 = url.replace(postGH, postGH_1)
+               r = requests.get(url_1)
+               if r.status_code != 200:
+                  continue
+               else:
+                  url = url_1
 
-    #github returns repos that do not exist, need to detect that here
-    '''
-    It threw error so I used a try--except block
-    There it says that the url does't exist
-    '''
-    #github when you give master instead of main, that might cause issues as well
-    urls = extractURLs(content)
-    dois = extractDOIs(content)
-    bibs = extractBibs(content) #for bonus
-    res = { 'ID': line, 'type': tp, 'url': url, 'content': content, 'links': urls, 'dois': dois, 'bibs': bibs }
-    out = json.dumps(res, ensure_ascii=False)
-    fo.write((out+"\n").encode())
-   except:
-    print("Doesn't exist")
-    continue
+         content = r.text
+   #github returns repos that do not exist, need to detect that here
+   #github when you give master instead of main, that might cause issues as well
+   #fixed both
+         urls = extractURLs(content)
+         dois = extractDOIs(content)
+         bibs = extractBibs(content)
+         res = { 'ID': line, 'type': tp, 'url': url, 'content': content, 'links': urls, 'dois': dois, 'bibs': bibs }
+         out = json.dumps(res, ensure_ascii=False)
+         fo.write((out+"\n").encode())
 
 run('model')
 
